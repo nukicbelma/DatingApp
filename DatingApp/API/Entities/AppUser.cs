cd0264921abd82141d2ddba7b50813caff3a1130
Belma Nukić <nukicbelma@hotmail.com>
2022-05-03 12:36:11 +0200

    Edit: atribut dateofbirth/calculateage

--- DatingApp/API/Entities/AppUser.cs
+++ DatingApp/API/Entities/AppUser.cs
@@ -12,7 +12,7 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte [] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-        public DateTime DateOdBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string Introduction { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
---
+++ DatingApp/API/Migrations/20220503103202_final.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOdBirth",
+                table: "Users",
+                newName: "DateOfBirth");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOfBirth",
+                table: "Users",
+                newName: "DateOdBirth");
+        }
+    }
+}
